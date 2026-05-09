b405d398ea9692cbbd20a7934e321a6e9e2a2e91
piales00 <perez.izquierdo.piero@gmail.com>
2026-05-09 13:28:26 -0500

    feat: add function transformApiData

---
+++ utils/transform.js
@@ -0,0 +1,18 @@
+import { getApiData } from "../services/api.js";
+
+export const transformApiData = (apiData) => {
+  const totalCountries = apiData.length;
+  const totalPopulation = apiData.reduce((population, country) => {
+    return population + country.population;
+  }, 0);
+  const countries = apiData.map((countrie) => ({
+    name: countrie.name.common,
+    population: countrie.population,
+  }));
+
+  return {
+    totalCountries: totalCountries,
+    totalPopulation: totalPopulation,
+    countries: countries,
+  };
+};
