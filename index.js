d3f9ed50b7eb409908a7bc5ee4eec25a682c6f09
piales00 <perez.izquierdo.piero@gmail.com>
2026-05-09 11:59:53 -0500

    refactor: replace variables with subregions

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 
-const countries = [
+const subregions = [
+  "Caribbean",
+  "Central America",
+  "Western Africa",
   "North America",
-  "Africa",
-  "Oceania",
+  "Eastern Africa",
+  "Polynesia",
   "South America",
-  "Europe",
-  "Asia",
-  "Antarctica",
+  "Micronesia",
+  "Southeast Europe",
+  "Northern Europe",
+  "Western Asia",
+  "Eastern Asia",
+  "Central Asia",
+  "Southern Europe",
+  "South-Eastern Asia",
+  "Central Europe",
+  "Northern Africa",
+  "Eastern Europe",
+  "Western Europe",
+  "Southern Asia",
+  "Middle Africa",
+  "Southern Africa",
+  "Melanesia",
+  "Australia and New Zealand",
 ];
+
 const rl = readline.createInterface({ input, output });
 console.log("\n╔════════════════════════════════════════════════════════╗");
 console.log("║    Bienvenido a la API de JS Fundamentals Latam   ║");
 console.log("╚════════════════════════════════════════════════════════╝\n");
 console.log("Selecciona un continente para conocer más detalles:\n");
-console.log(countries);
+console.log(subregions);
 
 const respuesta = await rl.question(
-  "¿Cuál continente quieres conocer? (Escribe el número correspondiente) \n1. North America \n2. Africa \n3. Oceania \n4. South America \n5. Europe \n6. Asia \n7. Antarctica\n",
+  "¿Cuál subregión quieres conocer? (Escribe el número correspondiente) \n1. Caribe \n2. América Central \n3. África Occidental \n4. América del Norte \n5. África Oriental \n6. Polinesia \n7. América del Sur \n8. Micronesia \n9. Europa del Sureste \n10. Europa del Norte \n11. Asia Occidental \n12. Asia Oriental \n13. Asia Central \n14. Europa del Sur \n15. Asia del Sureste \n16. Europa Central \n17. África del Norte \n18. Europa del Este \n19. Europa Occidental \n20. Asia del Sur \n21. África Central \n22. África del Sur \n23. Melanesia \n24. Australia y Nueva Zelanda\n",
 );
 rl.close();
 
-export const selectedContinent = countries[respuesta - 1];
+export const selectedRegion = subregions[respuesta - 1];
